a4b773f045b18668c7c6870e629b17203e582190
LadaZhyzneuskaya <trollll1349@gmail.com>
2023-01-02 23:40:14 +0300

    delete API-key

--- js/api.js
+++ js/api.js
@@ -5,8 +5,6 @@ async function getData(inputValue = 'Minsk') {
   try {
     // const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&units=metric&appid={API-key}`);  // API-key
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&units=metric&appid=74857689da28ac89a7df3c6a199eca74`);
-
     const data = await response.json();
     // console.log('DATA', data);
 
